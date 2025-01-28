5bbe8ddfb4351b409fb1a8529657a67b820bda1e
sadkingo <hker0004@gmail.com>
2025-01-28 19:18:59 +0100

    Adjusted contact strap

--- components/contact-strap/ContactStrap.component.tsx
+++ components/contact-strap/ContactStrap.component.tsx
@@ -1,22 +1,21 @@
 import { Center, ClipboardRoot, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import { ClipboardLink } from "../ui/clipboard";
-import { Icon } from "@iconify/react";
-import colors from "@/config/colors";
+import { BiPhoneCall } from "react-icons/bi";
 
 const ContactStrap = () => {
   const phoneNumber = "(555-555-555)";
   return (
-    <Center bg={{_light: colors.mediumLight, _dark: colors.medium}} gap={2} p={2}>
-      <Icon className="text-white" icon="line-md:phone-call-loop"/>
-      <Text color="white" fontWeight="bold">
+    <Center bg={{_light: "fg", _dark: "fg"}} w="vw" gap={2} p={2}>
+      <BiPhoneCall className="animate-wiggle"/>
+      <Text color="fg.inverted" fontWeight="bold">
         تحتاج مساعدة؟ متوفرون للمساعدة 24/7 :
       </Text>
       <Stack color="white">
         <ClipboardRoot value={phoneNumber}>
           <ClipboardLink
             value={phoneNumber}
-            color="black"
+            color="fg.inverted"
             fontWeight="bolder"
             textStyle="sm"
           />
