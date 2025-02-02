61496f59b08b25b69d693ecab0cf7d81995dd23b
sadkingo <hker0004@gmail.com>
2025-02-02 23:32:06 +0100

    Changed images render

--- components/carousel/Carousel.container.tsx
+++ components/carousel/Carousel.container.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react';
 import Carousel from "./Carousel.component";
 
-const carouselImages = Array.from({length: 19})
-  .map((_, index) => `/images/carousel/carousel-img-${index + 1}.jpg`);
+const NUMBER_OF_IMAGES = 20;
 
 const CarouselContainer: FC<
   {
@@ -10,6 +9,15 @@ const CarouselContainer: FC<
     reverseDirection?: boolean
   }
 > = (props) => {
+  const carouselImages = Array.from({length: NUMBER_OF_IMAGES - 1})
+    .map((_, index) => {
+      if (props.reverseDirection) {
+        const reversedIndex = ((12 - index + NUMBER_OF_IMAGES - 1) % NUMBER_OF_IMAGES) + 1;
+        return `/images/carousel/carousel-img-${reversedIndex}.jpg`;
+      }
+      return `/images/carousel/carousel-img-${index + 1}.jpg`
+    });
+
   return (
     <Carousel carouselImages={carouselImages} {...props}/>
   )
