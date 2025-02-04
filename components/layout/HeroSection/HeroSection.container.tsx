56a2e2cc76b8df18f88289bde7c4387834a9da75
sadkingo <hker0004@gmail.com>
2025-02-04 22:51:20 +0100

    Created hero section

---
+++ components/layout/HeroSection/HeroSection.container.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import HeroSection from "./HeroSection.component";
+
+const HeroSectionContainer: FC = () => {
+  return (
+    <HeroSection/>
+  )
+}
+
+export default HeroSectionContainer;
