f5bb19afb6a88f67c698aa00d3932e227e6bf754
sadkingo <hker0004@gmail.com>
2025-02-07 02:22:25 +0100

    Created features section

---
+++ components/layout/Features/Features.component.tsx
@@ -0,0 +1,67 @@
+import { FC } from 'react';
+import { Flex, Stack, Text } from "@chakra-ui/react";
+import { FaCheckCircle } from "react-icons/fa";
+
+const Features: FC = () => {
+  return (
+    <Flex
+      as="section"
+      justifyContent="center" gap={4}
+      w="full"
+      mb={3}
+      mdDown={{
+        flexDir: "column",
+        w: "fit",
+        m: "auto",
+        alignItems: "center",
+      }}
+    >
+      <Stack
+        justifyContent="center"
+        shadow="xl"
+        rounded="lg"
+        bg="bg.panel"
+        p={16}
+        fontSize={28} fontWeight="bold"
+        mdDown={{
+          w: "full",
+          alignItems: "center",
+        }}
+      >
+        <Text>دروس</Text>
+        <Text>واختبارات</Text>
+        <Text>بسهولة</Text>
+      </Stack>
+      <Stack
+        mdDown={{px: 8}}
+        shadow="xl"
+        rounded="lg"
+        bg="bg.panel"
+        p={8}
+        px={{base: 24, mdDown: 8}}
+        fontSize={28}
+        justifyContent="center"
+      >
+        <Flex gap={4} alignItems="center">
+          <FaCheckCircle color="lightgreen" className="text-green-400"/>
+          <Text>الاف الاختبارات بالحلول وبضغطة زر</Text>
+        </Flex>
+        <Flex gap={4} alignItems="center">
+          <FaCheckCircle color="lightgreen" className="text-green-400"/>
+          <Text>جميع المستويات الدراسية</Text>
+        </Flex>
+        <Flex gap={4} alignItems="center">
+          <FaCheckCircle color="lightgreen" className="text-green-400"/>
+          <Text>دروس وشروحات سهلة ومختصرة</Text>
+        </Flex>
+        <Flex gap={4} alignItems="center">
+          <FaCheckCircle color="lightgreen" className="text-green-400"/>
+          <Text>ملخصات ومراجعات سهلة وسريعة</Text>
+        </Flex>
+
+      </Stack>
+    </Flex>
+  )
+}
+
+export default Features;
---
+++ components/layout/Features/Features.container.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import Features from "./Features.component";
+
+const FeaturesContainer: FC = () => {
+  return (
+    <Features/>
+  )
+}
+
+export default FeaturesContainer;
