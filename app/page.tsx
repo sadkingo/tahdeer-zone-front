5abc213ae7c7b1c286f2f8f71271c304b656da6c
sadkingo <hker0004@gmail.com>
2025-02-09 23:42:40 +0100

    Added stat section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/components/layout/hero-section/HeroSection.component";
-import Features from "@/components/layout/Features/Features.component";
+import Features from "@/components/layout/features-section/FeaturesSection.component";
+import StatSection from "@/components/layout/stat-section/StatSection.component";
 
 export default function Home() {
   return (<>
