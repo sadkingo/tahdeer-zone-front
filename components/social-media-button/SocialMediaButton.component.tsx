64090e43c22e293267380622623458bdbb8072de
sadkingo <hker0004@gmail.com>
2025-05-19 16:00:56 +0100

    Created social media button

---
+++ components/social-media-button/SocialMediaButton.component.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react';
+import { Button } from "@/components/ui/button";
+
+const SocialMediaButton: FC<{ children: React.ReactNode }> = ({children}) => {
+  return (
+    <Button
+      _dark={{borderColor: "gray.600"}}
+      dir="ltr"
+      _hover={{color: "blue.600"}}
+      gap={8}
+      display="flex" justifyContent="start"
+      rounded="sm"
+      py={4}
+      px={12}
+      border="1px solid black"
+      lgDown={{w: "350px"}}
+    >
+      {children}
+    </Button>
+  )
+}
+
+export default SocialMediaButton;
---
+++ components/social-media-button/SocialMediaButton.container.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react';
+import SocialMediaButton from "./SocialMediaButton.component";
+
+const SocialMediaButtonContainer: FC = () => {
+  return (
+    <SocialMediaButton/>
+  )
+}
+
+export default SocialMediaButtonContainer;
